3bd6968a0bf711857fa8dfe67176b08a129395d3
andyhthai <andy.h.thai@gmail.com>
2026-03-08 15:56:22 -0700

    Refactor table retrieval to use year parameter
    Refactored get_table function to accept year as input and updated table extraction logic. Added example for 2025 and adjusted year assignment.

--- R/02_retrieve_tables.R
+++ R/02_retrieve_tables.R
@@ -3,81 +3,73 @@
 
 smashhtml <- read_html("https://liquipedia.net/smash/SSBMRank")
 
-tables <- smashhtml |> html_elements("table.sortable")
-
-filtered_tables <- tables[grepl("Rank|MPGR", tables |> html_text2())]
-
-filtered_tables <- tables[!grepl("Summer", filtered_tables |> html_text2())]
-
-tables <- filtered_tables |> html_table()
-
-# Possible values are 1-10 (2013 = 1, 2024 = 10) for table_index as of Nov 2025.
-get_table <- function(table_index) {
+# Enter the year you want for table_year
+get_table <- function(table_year) {
   # Extract the tables from html
-  tableshtml <- smashhtml |> html_elements("table.sortable")
+  tables <- smashhtml |> html_elements("table.sortable")
 
-  filtered_tables <- tableshtml[grepl("Rank|MPGR", tableshtml |> html_text2())]
+  filtered_tables <- tables[grepl("Rank|MPGR", tables |> html_text2())]
 
-  filtered_tables <- tableshtml[!grepl("Summer", tableshtml |> html_text2())]
+  filtered_tables <- filtered_tables[!grepl("Summer", filtered_tables |> html_text2())]
 
-  tables <- filtered_tables |> html_table()
+  if (table_year %in% c("2018", "2019")) {
+    extracted_table_html <- filtered_tables[grepl(paste("MPGR", table_year), filtered_tables |> html_text2())]
+  } else {
+    extracted_table_html <- filtered_tables[grepl(paste("SSBMRank", table_year), filtered_tables |> html_text2())]
+  }
 
-  # Get one table of your choosing
-  table <- tables[[table_index]]
+  extracted_table <- extracted_table_html |> html_table()
+
+  extracted_table <- extracted_table[[1]]
 
   # Make first row of that table to names
-  table <- table |> row_to_names(row_number = 1)
+  extracted_table <- extracted_table |> row_to_names(row_number = 1)
 
   # Make the first column name "rank"
-  colnames(table)[1] <- "rank"
+  colnames(extracted_table)[1] <- "rank"
 
   # Select only these columns if using 2013
-  if (table_index == 1) {
-    table <- table |> select(rank, Player, Score)
+  if (table_year == "2013") {
+    extracted_table <- extracted_table |> select(rank, Player, Score)
   }
 
   # Select only these columns for all else. Turn NEW into NA for ±
   else {
-    table <- table |> select(rank, Player, Score, `±`)
+    extracted_table <- extracted_table |> select(rank, Player, Score, `±`)
 
-    table <- table |> mutate(`±` = case_when(
+    extracted_table <- extracted_table |> mutate(`±` = case_when(
       `±` == "9000NEW" ~ NA,
       .default = as.numeric(`±`)
     ))
   }
 
   # Get the mains
-  mains <- filtered_tables[[table_index]] |>
+  mains <- extracted_table_html |>
     html_elements("td:nth-child(3) > img:nth-child(1)") |>
     html_attr("alt")
 
-  table <- table |> mutate(main = mains)
+  extracted_table <- extracted_table |> mutate(main = mains)
 
-  table <- clean_names(table)
+  extracted_table <- clean_names(extracted_table)
 
-  table$startgg <- table$player
+  extracted_table$startgg <- extracted_table$player
 
-  table <- table |> relocate(startgg, .after = player)
+  extracted_table <- extracted_table |> relocate(startgg, .after = player)
 
-  return(table)
+  extracted_table$year <- table_year
+
+  return(extracted_table)
 }
-
 # Example:
 
-table2016 <- get_table(4)
-table2017 <- get_table(5)
-table2018 <- get_table(6)
-table2019 <- get_table(7)
-table2022 <- get_table(8)
-table2023 <- get_table(9)
-table2024 <- get_table(10)
+table2016 <- get_table("2016")
+table2017 <- get_table("2017")
+table2018 <- get_table("2018")
+table2019 <- get_table("2019")
+table2022 <- get_table("2022")
+table2023 <- get_table("2023")
+table2024 <- get_table("2024")
+table2025 <- get_table("2025")
 
-table2016$year <- 2016
-table2017$year <- 2017
-table2018$year <- 2018
-table2019$year <- 2019
-table2022$year <- 2022
-table2023$year <- 2023
-table2024$year <- 2024
 
 ssbmtables <- rbindlist(list(table2016, table2017, table2018, table2019, table2022, table2023, table2024))
